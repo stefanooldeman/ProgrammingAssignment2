5412307230fc5aa5a0eade001017325293eb21f0
stefanooldeman <stefano.oldeman@gmail.com>
2015-02-21 19:47:17 +0100

    Add test scenario and imporve doc

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,17 @@
+###########################################################
+## Author: Stefano Oldeman
+## https://twitter.com/stefanooldeman
+##
+## cachematrix.R
 ## First an instance of makeCacheMatrix is created, then cacheSolve is called with a matrix and the cache
-## first time the cache is empty
+## the first time the cache is empty
 ## every other call is using the cache.
+## An example usage:
+data = matrix(c(2:7), nrow=2, ncol=2, byrow=TRUE)
+cache <- makeCacheMatrix()
+cacheSolve(data, cache)
+cacheSolve(data, cache) # this call will always be faster
+###########################################################
 
 ## makeCacheMatrix
 ##
@@ -27,9 +38,26 @@ cacheSolve <- function(matrix, cache) {
   calculated_value
 }
 
-# example
-data = matrix(c(2:7), nrow=2, ncol=2, byrow=TRUE)
-cache <- makeCacheMatrix()
-cacheSolve(data, cache)
-# call for second time will use cached value
-cacheSolve(data, cache)
+
+test <- function(k = 1000) {
+  print ("preparing large dataset for testcase")
+  ##### setup #######
+  library(MASS)
+  rho <- .3
+  S       <- matrix(rep(rho, k*k), nrow=k)
+  diag(S) <- 1
+  dat <- mvrnorm(10000, mu=rep(0,k), Sigma=S) ### be patient!
+  R <- cor(dat)
+  ##### retrieved from Stackoverflow: http://goo.gl/yoyR8S #######
+  print ("first call to cacheSolve (cache warmup, please wait..)")
+
+  cache <- makeCacheMatrix()
+  c1 <- system.time(RI1 <- cacheSolve(R, cache))
+  print (c1)
+  print("second call: expect time to be less")
+  c2 <- system.time(RI1 <- cacheSolve(R, cache))
+  print( c2)
+  c1[[3]] > c2[[3]]
+}
+
+test()
