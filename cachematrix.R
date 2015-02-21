382b854e3978433e11a0c2ebe11e2ce718127e32
stefanooldeman <stefano.oldeman@gmail.com>
2015-02-21 19:31:22 +0100

    Submit cacheSolver with documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First an instance of makeCacheMatrix is created, then cacheSolve is called with a matrix and the cache
+## first time the cache is empty
+## every other call is using the cache.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix
+##
+## @return a list of two methods
+##   save: stores a value to cache
+##   get: retrieves the value from cache
+makeCacheMatrix <- function(storage = NULL) {
+  list(get = function() storage,
+       save = function(val) storage <<- val)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve
+##
+## @return the inverse of a matrix
+##   when cache$get() is null, it's calculated. Otherwise a cache$get() is returned
+cacheSolve <- function(matrix, cache) {
+  cached_value <- cache$get()
+  if(!is.null(cached_value)) {
+    return(cached_value)
+  }
+  # calculate inverse
+  calculated_value <- solve(matrix)
+  cache$save(calculated_value)
+  calculated_value
 }
+
+# example
+data = matrix(c(2:7), nrow=2, ncol=2, byrow=TRUE)
+cache <- makeCacheMatrix()
+cacheSolve(data, cache)
+# call for second time will use cached value
+cacheSolve(data, cache)
